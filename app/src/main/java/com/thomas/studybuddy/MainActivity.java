cfb046414c317f12dc049a4827c7e34f5f7d14de
Thomas Lilly <thomaslilly65@gmail.com>
2017-04-15 18:37:12 -0400

    put finishing touches on login

--- app/src/main/java/com/thomas/studybuddy/MainActivity.java
+++ app/src/main/java/com/thomas/studybuddy/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
